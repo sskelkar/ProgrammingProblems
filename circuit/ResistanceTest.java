d247b8f673a467814a8aa27565fb790ea2eaf4be
Sojjwal Kelkar <binit7@gmail.com>
2018-02-28 07:41:03 +0530

    Create ResistanceTest.java

---
+++ circuit/ResistanceTest.java
@@ -0,0 +1,41 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ResistanceTest {
+
+    @Test
+    public void shouldAddUpSeriesResistances() {
+        Resistance r = new Resistance(3);
+
+        Resistance circuit = r.addInSeries(r);
+        assertEquals(6, circuit.getValue(), 0);
+    }
+
+    @Test
+    public void shouldCalculateParallelResistances() {
+        Resistance r = new Resistance(3);
+
+        Resistance circuit = r.addInParallel(r);
+        assertEquals(1.5, circuit.getValue(), 0);
+    }
+
+    @Test
+    public void shouldCalculateBothSeriesAndParallelResistances1() {
+        Resistance r = new Resistance(2);
+
+        Resistance circuit = r.addInParallel(r).addInSeries(r).addInSeries(r);
+
+        assertEquals(5, circuit.getValue(), 0);
+    }
+
+    @Test
+    public void shouldCalculateBothSeriesAndParallelResistances2() {
+        Resistance r1 = new Resistance(2);
+        Resistance r2 = new Resistance(4);
+
+        Resistance circuit = r1.addInSeries(r1).addInParallel(r2).addInSeries(r1);
+
+        assertEquals(4, circuit.getValue(), 0);
+    }
+}
