f19d12edaf893d002f300fd5795c292fe6641e33
sskelkar <skelkar@equalexperts.com>
2018-11-06 08:50:31 +0530

    project restructured

--- src/main/java/circuit/Resistance.java
+++ src/main/java/circuit/Resistance.java
@@ -1,3 +1,4 @@
+package circuit;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
--- src/test/java/circuit/ResistanceTest.java
+++ src/test/java/circuit/ResistanceTest.java
@@ -1,3 +1,5 @@
+package circuit;
+
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
