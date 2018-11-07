a63e8e70802cb5c266b9a72763107fca2cd22914
sskelkar <skelkar@equalexperts.com>
2018-11-07 15:25:24 +0530

    min depth of a binary tree

---
+++ src/main/java/binarytree/minDepth/MinDepth.java
@@ -0,0 +1,29 @@
+package binarytree.minDepth;
+
+import binarytree.BinaryTree;
+import binarytree.Node;
+
+public class MinDepth {
+    public int calculate(BinaryTree tree) {
+        return minDepth(tree.getRoot());
+    }
+
+    private int minDepth(Node curr) {
+        if (curr == null) return 0;
+
+        int left = minDepth(curr.left);
+        int right = minDepth(curr.right);
+
+        // cannot simply use Math.min because we need to ignore null branch
+        // find min of left and right while disregarding them if they are 0.
+        int min = left;
+        if (right > 0) {
+            if (min > 0) {
+                min = Math.min(min, right);
+            } else {
+                min = right;
+            }
+        }
+        return 1 + min;
+    }
+}
---
+++ src/test/java/binarytree/minDepth/MinDepthTest.java
@@ -0,0 +1,29 @@
+package binarytree.minDepth;
+
+import binarytree.BinaryTree;
+import org.junit.Test;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static org.junit.Assert.assertEquals;
+
+public class MinDepthTest {
+    @Test
+    public void calculateMinDepthOfATree() {
+        //given
+        BinaryTree tree = BinaryTree.from(newArrayList(
+                1, 2, 3, 4, 5, null, null, 6, null, 7, null, null, null, null, 8, null, null));
+
+        //then
+        assertEquals(2, new MinDepth().calculate(tree));
+    }
+
+    @Test
+    public void calculateMinDepthOfATree2() {
+        //given
+        BinaryTree tree = BinaryTree.from(newArrayList(
+                1, 2, null, null, 3, null, null));
+
+        //then
+        assertEquals(3, new MinDepth().calculate(tree));
+    }
+}
