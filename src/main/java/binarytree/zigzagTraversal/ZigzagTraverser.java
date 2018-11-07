d0a24630c7ab94ee1eda474c0e1119d0b9297d35
sskelkar <skelkar@equalexperts.com>
2018-11-07 14:49:39 +0530

    zigzag tree traversal

---
+++ src/main/java/binarytree/zigzagTraversal/ZigzagTraverser.java
@@ -0,0 +1,40 @@
+package binarytree.zigzagTraversal;
+
+import binarytree.BinaryTree;
+import binarytree.Node;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class ZigzagTraverser {
+    public List<Integer> traverseZigzag(BinaryTree tree) {
+        List<Integer> result = new ArrayList<>();
+        Stack<Node> s1 = new Stack<>();
+        Stack<Node> s2 = new Stack<>();
+        boolean leftFirst = false;
+
+        s1.push(tree.getRoot());
+        while (!s1.isEmpty()) {
+            Node curr = s1.pop();
+            result.add(curr.data);
+
+            if (leftFirst) {
+                if (curr.left != null) s2.push(curr.left);
+                if (curr.right != null) s2.push(curr.right);
+            } else {
+                if (curr.right != null) s2.push(curr.right);
+                if (curr.left != null) s2.push(curr.left);
+            }
+
+            if (s1.isEmpty()) {
+                Stack<Node> temp = s1;
+                s1 = s2;
+                s2 = temp;
+
+                leftFirst = !leftFirst;
+            }
+        }
+        return result;
+    }
+}
---
+++ src/test/java/binarytree/zigzagTraversal/ZigzagTraverserTest.java
@@ -0,0 +1,31 @@
+package binarytree.zigzagTraversal;
+
+import binarytree.BinaryTree;
+import org.junit.Test;
+
+import java.util.List;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
+
+public class ZigzagTraverserTest {
+    /* Tree structure:
+     *                              1
+     *                       2             3
+     *                  4          5 6           7
+     *             8        9 10
+     */
+    @Test
+    public void zigzagTraverseTheBinaryTree() {
+        //given
+        BinaryTree tree = BinaryTree.from(newArrayList(
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10, null, null, null, null, null, null, null, null, null, null, null));
+
+        //when
+        List<Integer> zigzagTraversalResult = new ZigzagTraverser().traverseZigzag(tree);
+
+        //then
+        assertEquals(asList(1, 2, 3, 7, 6, 5, 4, 8, 9, 10), zigzagTraversalResult);
+    }
+}
